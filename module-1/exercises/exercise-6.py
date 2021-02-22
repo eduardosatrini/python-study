d8f3b95190a7720eedfd74b56f975f406e2c3824
Eduardo Henrique <eduardosatrini@gmail.com>
2021-02-22 04:06:58 -0300

    exercise

---
+++ module-1/exercises/exercise-6.py
@@ -0,0 +1,8 @@
+w = float(input("Width: "))
+h = float(input("Height: "))
+
+area  = w * h
+paint = area / 2
+print("Dimension: {}x{} and Area: {}m²".format(w, h, area))
+print("liter of paint needed to clean the wall: {}".format(paint))
+
