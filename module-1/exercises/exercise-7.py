eec32cc7eee67c8f65a4f3c3a5e8cf522782c795
Eduardo Henrique <eduardosatrini@gmail.com>
2021-02-22 21:03:47 -0300

    exercise-7

---
+++ module-1/exercises/exercise-7.py
@@ -0,0 +1,5 @@
+value = float(input("Price of the product: $"))
+discount = value * 5 / 100
+prod = value - discount
+
+print("${:.2f} with 5%(${:.2f}) new value: ${:.2f}".format(value, discount, prod))
