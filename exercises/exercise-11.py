493f03e7e7c144caa5367e0e48230b724222be56
Eduardo Henrique <eduardosatrini@gmail.com>
2021-02-23 21:10:31 -0300

    exercise-12

---
+++ exercises/exercise-11.py
@@ -0,0 +1,4 @@
+value = float(input("Type a value: "))
+
+#print("Value: {} | Integer: {:.0f}".format(value, value))
+print("Value {} | Integer {}!".format(value, int(value)))
---
+++ exercises/exercise-12.py
@@ -0,0 +1,9 @@
+from math import hypot
+
+opposite = float(input("Opposite leg length: "))
+adjacent = float(input("Adjacent leg length: "))
+
+# hipotenuse = (opposite ** 2 + adjacent ** 2) ** (1/2)
+# print("The hypotenuse measure: {:.3f}".format(hipotenuse))
+
+print("The hypotenuse measure: {:.3f}".format(hypot(opposite, adjacent)))
