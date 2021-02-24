f905af470a43990d76ef09f03611062702ac1f99
Eduardo Henrique <eduardosatrini@gmail.com>
2021-02-24 04:01:25 -0300

    colors

---
+++ scripts/terminal-color.py
@@ -0,0 +1,22 @@
+
+# \033[0:30:40m
+
+# background red and font white
+# \033[0:30:41m
+print("\033[1;35;40mEduardo Henrique\033[m")
+
+# background blue and font yellow sublime 
+# \033[4:33:44m
+print("\033[1;36;43mJohn Doe\033[m")
+
+# background green and font white
+# \033[30:42m
+print("\033[1;42;40mMarie Curry\033[m")
+
+# background black and font white
+# \033[m
+print("\033[mSteve Joe\033[m")
+
+# background white and font black
+# \033[7:30m
+print("\033[7mFlynn Meat\033[m")
